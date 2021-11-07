194d045e04f5e29dbc4f2a5b685fce0e7bb352e0
Teguh <teguh.prabowo557@gmail.com>
2021-11-07 21:14:58 +0700

    🚩 implement response schema to user api

--- handler/user.go
+++ handler/user.go
@@ -2,23 +2,25 @@ package handler
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/copier"
 	"github.com/negadive/oneline/controller"
 	"github.com/negadive/oneline/schema"
 )
 
 func Register(c *fiber.Ctx) error {
-	_o := new(schema.UserRegisterReq)
+	req_body := new(schema.UserRegisterReq)
+	res_body := new(schema.UserRegisterRes)
 
-	if err := c.BodyParser(_o); err != nil {
+	if err := c.BodyParser(req_body); err != nil {
 		return err
 	}
 
-	o, err := controller.Register(_o)
+	o, err := controller.Register(req_body)
 	if err != nil {
 		return err
 	}
 
-	return c.Status(201).JSON(fiber.Map{
-		"name": o.Name,
-	})
+	copier.Copy(&res_body, &o)
+
+	return c.Status(201).JSON(&res_body)
 }
--- schema/user.go
+++ schema/user.go
@@ -4,3 +4,8 @@ type UserRegisterReq struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
 }
+
+type UserRegisterRes struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
